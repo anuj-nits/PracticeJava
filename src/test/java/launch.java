3387d547bcf63256cdad0259c8e7468ff847cbcf
anuj gupta <anuj.gupta1@sterlite.com>
2019-08-26 11:11:09 +0530

    .

--- src/test/java/launch.java
+++ src/test/java/launch.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class launch {
@@ -13,7 +14,9 @@ public class launch {
     public void testing() throws Exception {
 
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("http://google.com");
         Thread.sleep(3000);
